6863b8f61492d73cd4f1b890d4326707c83528db
jason <j.s.pursey@gmail.com>
2020-09-06 17:31:33 +1000

    Add image and price service

---
+++ image-service/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+var db = make(map[string]string)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.GET("/products/:sku", func(c *gin.Context) {
+		sku := c.Params.ByName("sku")
+		image, err := getImage(sku)
+		if err != nil {
+			c.JSON(http.StatusNotFound, "unknown SKU")
+		} else {
+			c.JSON(http.StatusOK, gin.H{
+				"imagePath": image,
+			})
+		}
+	})
+	r.Run()
+}
+
+func init() {
+	db["IPOD-BLK"] = "https://i.ebayimg.com/images/i/162486149300-0-1/s-l1000.jpg"
+	db["IPOD-RED"] = "https://static.decalgirl.com/assets/items/ipc/800/ipc-ss-red.jpg"
+	db["IPOD-WHT"] = "https://gladallover.files.wordpress.com/2011/12/ipod-classic-white.jpg"
+}
+
+func getImage(sku string) (string, error) {
+	if sku == "" {
+		return "", errors.New("empty sku")
+	}
+	image := db[sku]
+	if image == "" {
+		return "", errors.New("invalid sku")
+	}
+	return image, nil
+}
---
+++ price-service/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+var db = make(map[string]int)
+
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.GET("/products/:sku", func(c *gin.Context) {
+		sku := c.Params.ByName("sku")
+		price, err := getPrice(sku)
+		if err != nil {
+			c.JSON(http.StatusNotFound, "unknown SKU")
+		} else {
+			c.JSON(http.StatusOK, gin.H{
+				"price":          price,
+				"formattedPrice": fmt.Sprintf("AU$%.2f", float64(price)/float64(100)),
+			})
+		}
+	})
+	r.Run()
+}
+
+func init() {
+	db["IPOD-BLK"] = 19999
+	db["IPOD-RED"] = 29999
+	db["IPOD-WHT"] = 23999
+}
+
+func getPrice(sku string) (int, error) {
+	if sku == "" {
+		return 0, errors.New("empty sku")
+	}
+	price := db[sku]
+	if price == 0 {
+		return 0, errors.New("unknown sku")
+	}
+	return price, nil
+}
